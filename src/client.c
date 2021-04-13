32e360339dce748d16aca9b617b59231aab7382b
Eunice Amorim <up201904920@fe.up.pt>
2021-04-13 18:35:26 +0100

    Main to test make fifo util

--- src/client.c
+++ src/client.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include "common/utils.h"
 
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define NTHREADS 2
+
+void *func_1(void *a) {
+	make_fifo();
+	pthread_exit(NULL);	// no termination code
+	}
+
 int main() {
-  printf("hello world!");
-  make_fifo();
-  return 0;
-}
+	pthread_t id1;
+	if (pthread_create(&id1, NULL, func_1, NULL) != 0)
+		exit(-1);
+	pthread_join(id1, NULL);
+	return 0;
+  
+  }
