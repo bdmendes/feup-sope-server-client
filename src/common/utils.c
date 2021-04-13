db577d6c6348725dbad3ea0b35daf6fed1c4bf52
Eunice Amorim <up201904920@fe.up.pt>
2021-04-13 19:11:53 +0100

    Path name construction

--- src/common/utils.c
+++ src/common/utils.c
@@ -1,6 +1,14 @@
 #include "utils.h"
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
 
 void make_fifo(){
-    printf("\npi\n");
+    char buf[50];
+    pid_t pid = getpid();
+    pid_t tid = gettid();
+    snprintf(buf + strlen(buf), sizeof(buf), "/temp/%d.%d", pid, tid);
+
 };
