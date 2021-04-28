b69c590fb3143f029651b91bfd748e442cb3cddb
Eunice Amorim <up201904920@fe.up.pt>
2021-04-28 14:30:48 +0100

    Message doc

--- src/common/message/message.h
+++ src/common/message/message.h
@@ -3,14 +3,29 @@
 
 #include <sys/types.h>
 
+/**
+ * @brief Structure to store a message information
+ */
 typedef struct {
-    int rid;       // request id
-    pid_t pid;     // process id
-    pthread_t tid; // thread id
-    int tskload;   // task load
-    int tskres;    // task result
+    /** request id */
+    int rid; 
+    /** process id */      
+    pid_t pid;    
+    /** thread id */
+    pthread_t tid; 
+    /** task load */
+    int tskload;  
+    /** task result */
+    int tskres;    
 } Message;
 
+/**
+ * @brief Function to build a message
+ * @param message Pointer for return the assembled message
+ * @param request_id Resquest id of the 
+ * @param load
+ * @param answer
+ */
 void assemble_message(Message *message, int request_id, int load, int answer);
 
 #endif
