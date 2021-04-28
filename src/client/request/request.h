71a7b00368cc88af0c895cb5293c0461bbdcde1a
kat-maq <laimirandasantos@gmail.com>
2021-04-28 15:01:50 +0100

    Add request documentation

--- src/client/request/request.h
+++ src/client/request/request.h
@@ -4,15 +4,41 @@
 #include <time.h>
 #include <stdbool.h>
 
+
+/**
+ * @brief Struct which encapsulates the request attributes.
+ */
 typedef struct {
+    /*
+    * task load
+    */
     int load;
+    /*
+    * Request ID
+    */
     int rid;
+    /*
+    * Time until the thread give up waiting a response.
+    */
     struct timespec private_fifo_timeout;
+    /*
+    * Public fifo's file descriptor.
+    */
     int public_fifo_fd;
 } Request;
 
+/**
+ * @brief Checks if server is closed.
+ * 
+ * @return true if it is closed, false otherwise
+ */
 bool is_server_closed();
 
+/**
+ * @brief Send a request to the server and read its response, logging all the relevant operations. 
+ * 
+ * @param arg information to be send in the request.
+ */
 void *request_server(void *arg);
 
 #endif
