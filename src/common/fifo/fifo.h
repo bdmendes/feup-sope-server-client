3ec3319bf05387b9ac2bd53dda16a9879dbc0470
Eunice Amorim <up201904920@fe.up.pt>
2021-04-28 10:30:41 +0100

    Fifo Doc

--- src/common/fifo/fifo.h
+++ src/common/fifo/fifo.h
@@ -3,6 +3,12 @@
 
 #include <sys/types.h>
 
+/**
+ * @brief Function to get the name of the private fifo accoording to pid and tid
+ * @param buf Buffer ti return the name
+ * @param pid pid of the client process
+ * @param tid Posix id of the client thread
+ */
 void get_private_fifo_name(char buf[], pid_t pid, pthread_t tid);
 
 #endif
