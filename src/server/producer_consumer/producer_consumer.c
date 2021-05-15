03d8698845cb766abe3a4beea5d847ca1b302474
Bruno Mendes <brod56@sapo.pt>
2021-05-15 10:21:38 +0100

    Add producer thread

--- src/server/producer_consumer/producer_consumer.c
+++ src/server/producer_consumer/producer_consumer.c
@@ -1,5 +1,6 @@
 #include "producer_consumer.h"
 #include "../message_queue/message_queue.h"
+#include "../lib/lib.h"
 #include <stdio.h>
 #include <pthread.h>
 
@@ -8,7 +9,7 @@ static MessageQueue* ready;
 static unsigned max_pending_size = 0;
 static pthread_mutex_t pending_mutex = PTHREAD_MUTEX_INITIALIZER;
 static pthread_cond_t pending_cond = PTHREAD_COND_INITIALIZER;
-//static pthread_mutex_t ready_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t ready_mutex = PTHREAD_MUTEX_INITIALIZER;
 //static pthread_cond_t ready_cond = PTHREAD_COND_INITIALIZER;
 
 int init_producer_consumer(unsigned buffer_size){
@@ -28,11 +29,26 @@ int push_pending_request(const Message* message){
         pthread_cond_wait(&pending_cond, &pending_mutex); // TO DO: check for timeout
     }
     message_queue_push(pending, message);
+    pthread_cond_broadcast(&pending_cond);
     pthread_mutex_unlock(&pending_mutex);
     return 0;
 }
 
 void* producer(void* arg){
+    // TO DO: check for return values and cleanup routines
+    pthread_mutex_lock(&pending_mutex);
+    while (message_queue_empty(pending)){
+        pthread_cond_wait(&pending_cond, &pending_mutex); // TO DO: check for timeout
+    }
+    Message message = message_queue_front(pending);
+    message_queue_pop(pending);
+    pthread_mutex_unlock(&pending_mutex);
+    int answer = task(message.tskload);
+    message.tskres = answer; // it is up to the consumer to change the thread and process ids
+                             // after popping from the ready queue, after gathering the private fifo info to send
+    pthread_mutex_lock(&ready_mutex);
+    message_queue_push(ready, &message); // should we also limit the ready queue size?
+    pthread_mutex_unlock(&ready_mutex);
     pthread_exit(NULL);
 }
 
