e49ad475ca4cd8f34b2b164dbdb3fb2620f6133b
Bruno Mendes <brod56@sapo.pt>
2021-05-20 19:11:05 +0100

    Fix message queue bug

--- src/server/message_queue/message_queue.c
+++ src/server/message_queue/message_queue.c
@@ -1,20 +1,20 @@
 #include "message_queue.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 struct MessageQueueNode {
     Message element;
-    struct MessageQueueNode* next;
+    struct MessageQueueNode *next;
 };
 
 struct MessageQueue {
-    struct MessageQueueNode* head;
+    struct MessageQueueNode *head;
     unsigned size;
 };
 
-MessageQueue* init_message_queue(){
-    MessageQueue* queue = malloc(sizeof(struct MessageQueue));
-    if (queue == NULL){
+MessageQueue *init_message_queue() {
+    MessageQueue *queue = malloc(sizeof(struct MessageQueue));
+    if (queue == NULL) {
         fprintf(stderr, "Could not allocate message queue\n");
         return NULL;
     }
@@ -23,30 +23,35 @@ MessageQueue* init_message_queue(){
     return queue;
 }
 
-int message_queue_push(MessageQueue* queue, const Message* msg){
-    if (queue == NULL){
+int message_queue_push(MessageQueue *queue, const Message *msg) {
+    if (queue == NULL) {
         fprintf(stderr, "Message queue is null\n");
         return -1;
     }
-    struct MessageQueueNode* node = (struct MessageQueueNode*)
-        malloc(sizeof(struct MessageQueueNode));
-    if (node == NULL){
+
+    struct MessageQueueNode *node =
+        (struct MessageQueueNode *)malloc(sizeof(struct MessageQueueNode));
+    if (node == NULL) {
         fprintf(stderr, "Could not allocate message queue node\n");
         return -1;
     }
+
     node->element = *msg;
     node->next = NULL;
-    if (queue->head == NULL){
+    if (queue->head == NULL) {
         queue->head = node;
     } else {
-        queue->head->next = node;
+        struct MessageQueueNode *curr = queue->head;
+        while (curr->next != NULL)
+            curr = curr->next;
+        curr->next = node;
     }
     queue->size++;
     return 0;
 }
 
-int message_queue_pop(MessageQueue* queue){
-    if (queue->head == NULL){
+int message_queue_pop(MessageQueue *queue) {
+    if (queue->head == NULL) {
         fprintf(stderr, "Message queue is empty\n");
         return -1;
     }
@@ -56,20 +61,20 @@ int message_queue_pop(MessageQueue* queue){
     return 0;
 }
 
-unsigned message_queue_size(const MessageQueue* queue){
+unsigned message_queue_size(const MessageQueue *queue) {
     return queue->size;
 }
 
-Message message_queue_front(const MessageQueue* queue){
+Message message_queue_front(const MessageQueue *queue) {
     return queue->head->element;
 }
 
-bool message_queue_empty(const MessageQueue* queue){
+bool message_queue_empty(const MessageQueue *queue) {
     return queue->head == NULL;
 }
 
-void destroy_message_queue(MessageQueue* queue){
-    while(!message_queue_empty(queue)){
+void destroy_message_queue(MessageQueue *queue) {
+    while (!message_queue_empty(queue)) {
         message_queue_pop(queue);
     }
     free(queue);
