d18c3cf8eec943c3584c0e2a79bbdcb08a5af947
Bruno Mendes <brod56@sapo.pt>
2021-05-20 16:41:51 +0100

    Fix parser

--- src/server/parser/parser.c
+++ src/server/parser/parser.c
@@ -11,16 +11,17 @@ bool valid_server_options(int argc, char **argv) {
 
     if (argc != 4 && argc != 6)
         return false;
-
+    bool non_optional_arg = false;
     int opt = 0;
     while ((opt = getopt(argc, argv, "t:l:")) != -1) {
         switch (opt) {
             case 't':
-                if (!is_all_digits(optarg) || atoi(optarg) == 0 || optind != 3)
+                if (!is_all_digits(optarg) || optind != 3)
                     return false;
+                non_optional_arg = true;
                 break;
             case 'l':
-                if (!is_all_digits(optarg) || optind != 5)
+                if (!is_all_digits(optarg) || optind != 5 || !non_optional_arg)
                     return false;
                 break;
             default:
