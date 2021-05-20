e288edc5699f79f38bc90fb622df845dccd1f252
Bruno Mendes <brod56@sapo.pt>
2021-05-20 08:51:50 +0100

    Remove unneeded check

--- src/server/parser/parser.c
+++ src/server/parser/parser.c
@@ -1,30 +1,30 @@
-#include <unistd.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <stdbool.h>
+#include <unistd.h>
 
-#include "parser.h"
 #include "../../common/utils/utils.h"
+#include "parser.h"
 
-bool valid_server_options(int argc, char **argv){
-    
-    if(argc != 4 && argc != 6) return false;
+bool valid_server_options(int argc, char **argv) {
+
+    if (argc != 4 && argc != 6)
+        return false;
 
     int opt = 0;
-    bool hasNonOptionalArg = false;
-
     while ((opt = getopt(argc, argv, "t:l:")) != -1) {
         switch (opt) {
-        case 't':
-            if (!is_all_digits(optarg) || optind != 3) return false;
-            hasNonOptionalArg = true;
-            break;
-        case 'l':
-            if (!is_all_digits(optarg) || optind != 5 || !hasNonOptionalArg) return false;
-            break;
-        default:
-            return false;
+            case 't':
+                if (!is_all_digits(optarg) || optind != 3)
+                    return false;
+                break;
+            case 'l':
+                if (!is_all_digits(optarg) || optind != 5)
+                    return false;
+                break;
+            default:
+                return false;
         }
     }
 
